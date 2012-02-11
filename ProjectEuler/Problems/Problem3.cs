bf3ebb2ce825fe1ba2d134ac593412d9c06a6d94
Gideon Israel Dsouza <gidisrael@gmail.com>
2012-02-11 11:53:50 +0530

    Structured project and added comments and links

--- ProjectEuler/Problems/Problem3.cs
+++ ProjectEuler/Problems/Problem3.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+ * http://projecteuler.net/problem=3
+ * */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- ProjectEuler/Problems/Problem4.cs
+++ ProjectEuler/Problems/Problem4.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+ * http://projecteuler.net/problem=4
+ * 
+ * */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
