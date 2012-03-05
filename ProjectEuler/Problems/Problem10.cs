f8528168d0a95db22059fa3926d4192a54858752
Gideon Israel Dsouza <gidisrael@gmail.com>
2012-03-05 21:16:28 +0530

    Added explanation of problem 10

--- ProjectEuler/Problems/Problem10.cs
+++ ProjectEuler/Problems/Problem10.cs
@@ -25,9 +25,9 @@ namespace ProjectEuler.Problems
             bool[] e = new bool[n];//by default they're all false
             for (int i = 2; i < n; i++)
             {
-                e[i] = true;
+                e[i] = true;//set all numbers to true
             }
-
+            //weed out the non primes by finding mutiples 
             for (int j = 2; j < n; j++)
             {
                 if (e[j])//is true
@@ -38,6 +38,11 @@ namespace ProjectEuler.Problems
                     }
                 }
             }
+            //Uptill here e[] sorta of contains a list of primes
+            //the index represent the actual number and the value at the index represents if the number is prime
+            //Example:
+            //e[4], e[100] will all be false since 2,4,100 are not primes
+            //e[5], e[7], e[11], e[13] will all be true because 5,7,11,13 are all prime numbers
             for (long r = 2; r < n; r++)
             {
                 if (e[r])
