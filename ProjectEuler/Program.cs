ba4361843dbf27c872075cb1a56d0c583c5fd0ce
Gideon Israel Dsouza <gidisrael@gmail.com>
2012-03-08 16:15:20 +0530

    Done problem 12

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -11,7 +11,7 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            RunProblem(new Problem11());
+            RunProblem(new Problem12());
         }
         static void RunProblem(IProblem prob)
         {
